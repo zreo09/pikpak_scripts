099a8db4b03dc0c44cbd65fec7c77f806b17e132
Malaaaa <37627837+Malaaaa@users.noreply.github.com>
2024-04-07 08:36:31 -0400

    use mail.tm api

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-const {PikPak} = require("./pikpak");
-const randomstring = require('randomstring')
+const { PikPak } = require("./pikpak");
+const randomstring = require("randomstring");
+const axios = require("axios");
+const Mailjs = require("@cemalgnlts/mailjs");
+const mailjs = new Mailjs();
 
 async function main() {
-    const name = randomstring.generate({
-        length: 8,
-        charset: 'alphabetic'
-    });
-    const password = randomstring.generate({
-        length: 10,
-        charset: 'alphanumeric'
-    });
+  const response = await mailjs.createOneAccount();
+  const username = response.data.username;
+  const password = response.data.password;
 
-    // 注册
-    let pikPak = new PikPak(`${name}@temp.com`, password, null, '自己的邀请码');
-    pikPak.authVerification()
-        .then(() => pikPak.getVerificationCode())
-        .then(() => pikPak.authVerificationVerify())
-        .then(() => pikPak.authSignup())
-        .then(() => pikPak.userMe())
-        .then(() => pikPak.activityInvite())
-        .then(() => pikPak.activityInviteCode())
-        .then(() => pikPak.activationCode())
-        .then(() => pikPak.printUserInfo())
-        .catch(e => console.log(e))
+  console.log(username, password);
+  let pikPak = new PikPak(username, password, null, "自己的邀请码");
 
-    // 登录
-    // let pikPak = new PikPak('temp@temp.com', 'tempPassword', 'tempuuid');
-    // pikPak.signin()
-    //     .then(() => pikPak.userMe())
-    //     .then(() => pikPak.printUserInfo())
-    //     .catch(e => console.log(e))
+  pikPak
+    .authVerification()
+    .then(() => pikPak.getVerificationCode(mailjs))
+    .then(() => pikPak.authVerificationVerify())
+    .then(() => pikPak.authSignup())
+    .then(() => pikPak.userMe())
+    .then(() => pikPak.activityInvite())
+    .then(() => pikPak.activityInviteCode())
+    .then(() => pikPak.activationCode())
+    .then(() => pikPak.printUserInfo())
+    .catch((e) => console.log(e));
+
+  // 登录
+  // let pikPak = new PikPak('temp@temp.com', 'tempPassword', 'tempuuid');
+  // pikPak.signin()
+  //     .then(() => pikPak.userMe())
+  //     .then(() => pikPak.printUserInfo())
+  //     .catch(e => console.log(e))
 }
 
-main()
+main();
